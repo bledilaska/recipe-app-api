fd3b49f5b8bc3df76e3a0b93e2e9115dd268ff68
bledilaska <laskabledi@gmail.com>
2022-07-23 20:19:16 +0200

    Configured Django

---
+++ app/app/calc.py
@@ -0,0 +1,10 @@
+""
+Calculator functions
+""
+
+def add(x,y):
+    """Add x and Y and return result."""
+    return x + y
+def subtract (x,y):
+    """Substract x from y and return result."""
+    return y -x 
